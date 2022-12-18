6a0ecb1e62d2dfb957ecf6b773dc0ec6818732f3
jltdev15 <75570239+jltdev15@users.noreply.github.com>
2022-12-18 21:43:40 +0800

    add services section

--- projects/techguru/script.js
+++ projects/techguru/script.js
@@ -2,15 +2,22 @@ const toggleButton = document.getElementsByClassName('toggle__button')[0]
 const navbarlinks = document.getElementsByClassName('nav-links')[0]
 const toggleCat = document.getElementsByClassName('toggle_category')[0]
 const heroToggle = document.getElementsByClassName('')
+const teamToggle = document.getElementsByClassName('toggle__team')[0]
+
+
 
 
 toggleButton.addEventListener('click', () => {
     navbarlinks.classList.toggle('active');
 })
 
-toggleCat.addEventListener('click', () => {
-    navbarlinks.classList.remove('active');
-})
+// toggleCat.addEventListener('click', () => {
+//     navbarlinks.classList.remove('active');
+// })
 heroToggle.addEventListener('click', () => {
-    navbarlinks.classList.remove('active');
+    navbarlinks.classList.toggle('active');
+})
+
+teamToggle.addEventListener('click', () => {
+    navbarlinks.classList.toggle('active');
 })
